e7c5fa32abafa6a02fcb31ad745aa12fd53ee5ba
replakcan <alper.makcan@gmail.com>
2025-04-13 23:45:34 +0300

    Updates number button order

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ calculator.classList.add('calculator')
 
 body.appendChild(calculator)
 
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
 const mathOperators = ['+', '-', '*', '/']
 
 const helperOperators = ['C', '=']
