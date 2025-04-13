977e4f15abf7cc2503dc116e0373c9b62f244154
replakcan <alper.makcan@gmail.com>
2025-04-13 23:31:30 +0300

    Updates helper operators variable name
    since these are not operations but operators, updated the name according to that

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ body.appendChild(calculator)
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 const mathOperators = ['+', '-', '*', '/']
 
-const helperOperations = ['C', '=']
+const helperOperators = ['C', '=']
 
 const resultScreen = document.createElement('div')
 resultScreen.classList.add('result-screen')
